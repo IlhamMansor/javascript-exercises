e167e6684b999db3bd6c2cf673e54c3399935160
IlhamMansor <ilhamthaqif98@gmail.com>
2022-02-19 21:17:40 +0800

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let repeatedString = "";
+  let i = 0;
+  while (i < num) {
+    repeatedString += string;
+    i++;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
