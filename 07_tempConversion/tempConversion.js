90785ec3375d547db802e5345dff44eed64aa0ae
IlhamMansor <ilhamthaqif98@gmail.com>
2022-02-24 00:26:25 +0800

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function (fahranheit) {
+    let celcius = (fahranheit - 32) * (5 / 9);
+    if (celcius == 0) {
+        return celcius;
+    } else {
+        return Math.round(celcius * 10) / 10;
+    }
 };
 
-const ctof = function() {
-
+const ctof = function (celcius) {
+    let fahranheit = celcius * (9 / 5) + 32;
+    if (fahranheit == 0) {
+        return fahranheit;
+    } else {
+        return Math.round(fahranheit * 10) / 10;
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
